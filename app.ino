9e9657dbf7aa408fcc3e0b880b7b80bb671199e9
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2020-02-19 21:34:37 +0000

    First commit - simple interrupt

---
+++ app.ino
@@ -0,0 +1,31 @@
+#include "heltec.h"
+#include "test.h"
+
+volatile bool InterruptState = false;
+
+static void buttonInterrupt()
+{
+	InterruptState = !InterruptState;
+}
+
+void setup()
+{
+	pinMode(LED,OUTPUT);
+	digitalWrite(LED,HIGH);
+
+	pinMode(KEY_BUILTIN, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(KEY_BUILTIN), buttonInterrupt, CHANGE);
+
+	Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+}
+
+void loop()
+{
+	digitalWrite(LED,HIGH);
+	Test(InterruptState ? "LED high (*)" : "LED high");
+	delay(2000);
+
+	digitalWrite(LED,LOW);
+	Test(InterruptState ? "LED low (*)" : "LED low");
+	delay(2000);
+}
